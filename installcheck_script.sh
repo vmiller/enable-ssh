b8bac9c06d77294764573da3529ef534ce9eaae7
Vaughn Miller <millerva@lafayette.edu>
2018-02-27 10:33:49 -0500

    use full path for systemsetup

--- installcheck_script.sh
+++ installcheck_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #check to see if ssh is off
-if [[ $(systemsetup -getremotelogin) = 'Remote Login: Off' ]] 
+if [[ $(/usr/sbin/systemsetup -getremotelogin) = 'Remote Login: Off' ]] 
 then
 	# exit 0 to tell Munki an install is needed
 	exit 0
